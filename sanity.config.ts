29e269b98f5385e073724fceb895bea53a755cc0
Daniel Nguyen <danyen125@gmail.com>
2023-04-09 15:48:35 -0700

    update projectId

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import schemas from "./sanity/schemas"
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
-  projectId: "txd29ab2",
-  dataset: "production",
-  title: "My Personal Website",
-  apiVersion: "2023-03-09",
-  basePath: "/admin",
-  plugins: [deskTool()],
-  schema: { types: schemas }
-})
+	projectId: '0e47t3xs',
+	dataset: 'production',
+	title: 'My Personal Website',
+	apiVersion: '2023-03-09',
+	basePath: '/admin',
+	plugins: [deskTool()],
+	schema: { types: schemas },
+});
 
-export default config
+export default config;
